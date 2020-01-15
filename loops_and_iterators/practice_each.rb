20891f072f2b33ecae86e3f8ff22612858d2c7cf
Elle Emmerich <ellenmmackey@gmail.com>
2020-01-15 02:21:28 +0000

    Chapter: Loops and Iterators

---
+++ loops_and_iterators/practice_each.rb
@@ -0,0 +1,7 @@
+names = ['Bob', 'Joe', 'Steve']
+x = 1
+
+names.each do |name|
+  puts  "#{x}. #{name}"
+  x += 1
+end
---
+++ loops_and_iterators/stop.rb
@@ -0,0 +1,9 @@
+puts "Keep going or STOP?"
+x = gets.chomp
+
+while x != "STOP"
+  puts "Keep going or STOP?"
+  x = gets.chomp
+end 
+
+puts "Good job!"
